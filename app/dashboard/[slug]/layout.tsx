a50bdbd22d1fcc2af7f43d1b0299b2c10d0dc207
Raymond Amoah <razbowy.70@gmail.com>
2026-02-02 04:24:11 +0000

    fixed layout for movie streaming

--- app/dashboard/[slug]/layout.tsx
+++ app/dashboard/[slug]/layout.tsx
@@ -10,18 +10,19 @@ interface iAppProps {
 export default async function CourseLayout({ children, params }: iAppProps) {
   const { slug } = await params;
 
-  // Server-side security check and lightweight data fetching
   const course = await getCourseSidebarData(slug);
 
   return (
-    <div className="flex flex-1">
-      {/* sidebar - 30% */}
-      <div className="w-80 border-r border-border shrink-0">
-        <CourseSidebar course={course.course} />
+    <div className="flex flex-col md:flex-row flex-1">
+      {/* MAIN CONTENT */}
+      <div className="order-1 md:order-2 flex-1 overflow-hidden">
+        {children}
       </div>
 
-      {/* Main Content - 70% */}
-      <div className="flex-1 overflow-hidden">{children}</div>
+      {/* SIDEBAR */}
+      <div className="order-2 md:order-1 w-full md:w-80 border-t md:border-t-0 md:border-r border-border shrink-0">
+        <CourseSidebar course={course.course} />
+      </div>
     </div>
   );
 }
